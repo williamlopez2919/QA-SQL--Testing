bef672a0ee739b0397a57eddeda1b98432eee575
williamlopez2919 <williamlopez2919@gmail.com>
2026-08-15 10:57:15 -0400

    Crear Base de Datos

--- database/01_create_database.sql
+++ database/01_create_database.sql
@@ -1,6 +1,15 @@
+-- ============================================================
+-- PROYECTO QA: Validación y Pruebas en Base de Datos
+-- Archivo: 01_create_database.sql
+-- Descripción: Creación de la base de datos principal
+-- Motor recomendado: PostgreSQL / MySQL
+-- ============================================================
+
 -- 1. Eliminar la base de datos si ya existe (para asegurar ejecuciones limpias en pruebas)
-drop database if exists tiendawilliam;
--- Crear base de datos 
-create database tiendawilliam; 
--- Seleccionar la base de datos 
-use tiendawilliam;
+DROP DATABASE IF EXISTS tienda_william;
+
+-- 2. Crear la base de datos
+CREATE DATABASE tienda_william;
+
+-- 3. usar base de datos
+USE tienda_william;
