d3d7f5ea1ef013ae062f7c6fc86269586469e372
williamlopez2919 <williamlopez2919@gmail.com>
2026-08-15 03:23:29 -0400

    insertar datos a una tabla

---
+++ database/03-insertar-datos.sql
@@ -0,0 +1,4 @@
+insert into empleados(nombre, edad, salario, fecha_ingreso) values 
+('william lopez',35,25000.00,'2026-07-01'),
+('Ayeisha Ramos',30,35000.00,'2020-05-10'),
+('Deyanira Parras',50,45000.00,'2021-10-21');
