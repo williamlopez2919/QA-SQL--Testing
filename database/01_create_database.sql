a0601ff550dae1395616e3ded68b5404b163ca8d
williamlopez2919 <williamlopez2919@gmail.com>
2026-08-15 03:51:26 -0400

    Crear Base de Datos
    Added a comment to clarify the purpose of dropping the database.

--- database/01_create_database.sql
+++ database/01_create_database.sql
@@ -1,3 +1,5 @@
+-- 1. Eliminar la base de datos si ya existe (para asegurar ejecuciones limpias en pruebas)
+drop database if exists tiendawilliam;
 -- Crear base de datos 
 create database tiendawilliam; 
 -- Seleccionar la base de datos 
